7f903e8607b4154ca4ea5a3868bf5c01146af454
stasha <contact@stasha.info>
2018-11-05 00:38:42 +0100

    Ignoring test

--- src/test/java/info/stasha/testosterone/junit4/TopSuiteTest.java
+++ src/test/java/info/stasha/testosterone/junit4/TopSuiteTest.java
@@ -2,12 +2,14 @@ package info.stasha.testosterone.junit4;
 
 import info.stasha.testosterone.annotation.Configuration;
 import info.stasha.testosterone.jersey.Testosterone;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 /**
  *
  * @author stasha
  */
+@Ignore
 @RunWith(TestosteroneSuiteRunner.class)
 @org.junit.runners.Suite.SuiteClasses({
 	RunTestsAndSuitesSuite.class
