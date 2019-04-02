ebb758ccb435c7449628e8837bc5b85fef7e8562
stasha <contact@stasha.info>
2019-04-02 23:08:28 +0200

    Updated runtime binding test

--- src/test/java/info/stasha/testosterone/jersey/junit4/runtimebinding/RuntimeBindingTest.java
+++ src/test/java/info/stasha/testosterone/jersey/junit4/runtimebinding/RuntimeBindingTest.java
@@ -3,7 +3,6 @@ package info.stasha.testosterone.jersey.junit4.runtimebinding;
 import info.stasha.testosterone.jersey.junit4.Testosterone;
 import info.stasha.testosterone.junit4.TestosteroneRunner;
 import javax.ws.rs.core.Context;
-import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.jersey.server.ResourceConfig;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
@@ -22,12 +21,12 @@ public class RuntimeBindingTest implements Testosterone {
     }
 
     @Context
-    private ServiceLocator locator;
+    private String stringFromFactory;
 
     @Test
     public void runtimeBindedFactoryTest() {
-        assertEquals("String from runtime binded factory should equal", 
-                RuntimeBindingFeature.STRING_FROM_RUNTIME_BINDED_FACTORY, locator.getService(String.class));
+        assertEquals("String from runtime binded factory should equal",
+                RuntimeBindingFeature.STRING_FROM_RUNTIME_BINDED_FACTORY, stringFromFactory);
     }
 
 }
